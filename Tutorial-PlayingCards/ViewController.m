dfd1acc3194649857da94daefa85f48554083bdc
sans-connaissance <davemalicke@gmail.com>
2022-07-01 07:37:10 -0400

    ADD flipCount label and logic

--- Tutorial-PlayingCards/ViewController.m
+++ Tutorial-PlayingCards/ViewController.m
@@ -9,13 +9,30 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)setFlipCount:(int)flipCount {
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    NSLog(@"flipCount = %d", self.flipCount);
+}
+
+- (IBAction)touchCardButton:(UIButton *)sender {
+    
+    if ([sender.currentTitle length]) {
+        UIImage *cardImage = [UIImage imageNamed:@"stanford"];
+        [sender setBackgroundImage: cardImage forState: UIControlStateNormal];
+        [sender setTitle:@"" forState: UIControlStateNormal];
+    } else {
+        [sender setTitle:@"A♧" forState: UIControlStateNormal];
+    }
+    self.flipCount++;
+
 }
 
 
